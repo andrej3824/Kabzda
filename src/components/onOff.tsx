bf6215efd680315da89705b458fbf4f1259fb433
andrej3824 <an.drej@mail.ru>
2021-09-29 18:20:04 +0200

    onOff

---
+++ src/components/onOff.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+type PropsType = {
+    on: boolean
+}
+
+function OnOff(props: PropsType)  {
+
+    const onStyle = {}
+    const offStyle = {}
+    const indicatorStyle = {
+        width: "10px",
+        height: "10px",
+        borderRadius: "5px",
+        border: "1px solid black"
+    }
+
+
+    return <div>
+        <div>on</div>
+        <div>off</div>
+        <div style={indicatorStyle}></div>
+    </div>
+}
+
+export default OnOff
