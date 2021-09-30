bdd1a8a6a31cc9ad8745debc101d4f0cdb265e61
andrej3824 <an.drej@mail.ru>
2021-09-30 15:36:59 +0200

    onOff

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import OnOff from "./components/onOff";
 import UncontrolledAccordion from "./components/UncontrolledAccordion";
-import {Rating} from "./components/Rating";
+import {Rating, RatingValueType} from "./components/Rating";
 import {UncontrolledRating} from "./components/UncontrolledRating";
 
 function App() {
+
+    let [ratingValue, setRatingValue] = useState<RatingValueType>(0)
+
     return (
         <div className={"App"}>
 
             <OnOff/>
             <UncontrolledAccordion titleValue={'Menu'}/>
-            <UncontrolledRating />
-            <Rating value={3}/>
+            <UncontrolledRating value={0}/>
+            <Rating value={ratingValue} onClick={setRatingValue}/>
         </div>
     );
 }
--- src/components/UncontrolledRating.tsx
+++ src/components/UncontrolledRating.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 
 type RatingPropsType = {
-    //value: 0 | 1 | 2 | 3 | 4 | 5
+    value: 0 | 1 | 2 | 3 | 4 | 5
 }
 
 export function UncontrolledRating(props: RatingPropsType) {
